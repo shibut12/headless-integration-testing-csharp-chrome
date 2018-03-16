dfd0abb76e732de35fa1fddb96e1908aab8a7468
shibut12 <shibu@hotmail.co.in>
2018-03-16 00:22:34 -0400

    Updated chrome driver options to enable testing in linux environment

--- SutGoogleHomePage.cs
+++ SutGoogleHomePage.cs
@@ -12,6 +12,12 @@ namespace HeadLessTest
         {
             var args = new ChromeOptions();
             args.AddArgument("--headless");
+            args.AddArgument("--no-sandbox");
+            args.AddArgument("--ignore-ssl-errors=true");
+            args.AddArgument("--ssl-protocol=any");
+            args.AddArgument("--disable-gpu");
+            args.AddArgument("window-size=1400,600");
+
             //var driver = new ChromeDriver(args);
             var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), args);
             driver.Url = "https://www.google.com";
